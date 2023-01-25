9f371ae996dd07e8deb6ba88a27efc55fbcd3ed7
solokool66 <solokool66@gmail.com>
2023-01-25 05:15:49 -0800

    The printf files corrected

--- get_flag.c
+++ get_flag.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * cal_active_flags - Calculates active flags
+ * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
-int cal_active_flags(const char *format, int *i)
+int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
