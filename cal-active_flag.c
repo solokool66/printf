68ffd0b49f205cfe620c3071f7c00ef8636039e1
solokool66 <solokool66@gmail.com>
2023-01-25 04:24:33 -0800

    my cal-active flag corrected

--- cal-active_flag.c
+++ cal-active_flag.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * cal-active_flags - Calculates active flags
+ * cal_active_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
-int cal-active_flags(const char *format, int *i)
+int cal_active_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
